7713dd89c83308d035c64a9877f1f98c821380c5
Tache-Stefan <stefantache@ymail.com>
2024-10-19 12:09:24 +0300

    Added the classes Song, Playlist and Artist.

--- main.cpp
+++ main.cpp
@@ -1,136 +1,114 @@
+#pragma once
+
 #include <iostream>
-#include <array>
-#include <chrono>
-#include <thread>
+#include <string>
+#include <vector>
 
-#include <SFML/Graphics.hpp>
+class Song;
+class Playlist;
+class Artist;
 
-#include <Helper.h>
+struct time {
+    int hours;
+    int minutes;
+    int seconds;
+};
+//TODO: implementare length cu struct asta
 
-//////////////////////////////////////////////////////////////////////
-/// NOTE: this include is needed for environment-specific fixes     //
-/// You can remove this include and the call from main              //
-/// if you have tested on all environments, and it works without it //
-#include "env_fixes.h"                                              //
-//////////////////////////////////////////////////////////////////////
-
-
-//////////////////////////////////////////////////////////////////////
-/// This class is used to test that the memory leak checks work as expected even when using a GUI
-class SomeClass {
+class Song {
+private:
+    std::string title;
+    float length;
+    std::vector<Artist> artists;
 public:
-    explicit SomeClass(int) {}
+    Song(const std::string& title_, const float length_, std::vector<Artist> artists_) : title(title_), length(length_), artists(artists_) {
+        std::cout << "Song(+artist): " << title << std::endl;
+    }
+    Song(const std::string& title_, const float length_) : title(title_), length(length_) {
+        std::cout << "Song: " << title << std::endl;
+    }
+    Song(const Song& other) : title(other.title), length(other.length), artists(other.artists) {
+        std::cout << "Song(copiere)" << std::endl;
+    }
+    Song& operator=(const Song& other) {
+        title = other.title;
+        length = other.length;
+        artists = other.artists;
+        std::cout << "Song(operator=)" << std::endl;
+        return *this;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Song& song) {
+        os << "Song: " << song.title << ", " << song.length << "\n";
+        return os;
+    }
+    ~Song() {std::cout << "Song(destr)" << std::endl; }
+    float getLength() const {return length;}
+    void addArtist(const Artist& artist) {
+        artists.push_back(artist);
+    }
 };
 
-SomeClass *getC() {
-    return new SomeClass{2};
-}
-//////////////////////////////////////////////////////////////////////
+class Playlist {
+private:
+    std::string title;
+    std::vector<Song> songs;
+    float length;
+public:
+    Playlist(const std::string& title_, std::vector<Song> songs_) : title(title_), songs(songs_) {
+        std::cout << "Playlist: " << title << "\n";
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Playlist& playlist) {
+        os << "Playlist: " << playlist.title << "\n";
+        return os;
+    }
+    float getLength() const {return length;}
+    void calculateLength() {
+        float aux = 0;
+        for(auto& song : songs)
+            aux += song.getLength();
+        length = aux;
+    }
+};
 
+class Artist {
+private:
+    std::string name;
+    std::vector<Song> songs;
+public:
+    Artist(const std::string& name_, std::vector<Song> songs_) : name(name_), songs(songs_) {
+        std::cout << "Artist(+songs): " << name << std::endl;
+    }
+    explicit Artist(const std::string& name) {
+        std::cout << "Artist: " << name << std::endl;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Artist& artist) {
+        os << "Artist: " << artist.name << std::endl;
+        return os;
+    }
+    void addSong(const Song& song) {
+        songs.push_back(song);
+    }
+};
 
 int main() {
-    ////////////////////////////////////////////////////////////////////////
-    /// NOTE: this function call is needed for environment-specific fixes //
-    init_threads();                                                       //
-    ////////////////////////////////////////////////////////////////////////
-    ///
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
+    Song TalkingToTheMoon{"Talking to the moon", 3.35};
+    Song DieWithASmile{"Die with a smile", 4.13};
+    Artist BrunoMars{"Bruno Mars"};
+    Artist LadyGaga{"Lady Gaga"};
 
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;
+    BrunoMars.addSong(TalkingToTheMoon);
+    BrunoMars.addSong(DieWithASmile);
+    LadyGaga.addSong(DieWithASmile);
+    TalkingToTheMoon.addArtist(BrunoMars);
+    DieWithASmile.addArtist(BrunoMars);
+    DieWithASmile.addArtist(LadyGaga);
 
-    sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                                    ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
+    std::vector<Song> songs;
+    songs.push_back(TalkingToTheMoon);
+    songs.push_back(DieWithASmile);
 
-    while(window.isOpen()) {
-        bool shouldExit = false;
-        sf::Event e{};
-        while(window.pollEvent(e)) {
-            switch(e.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-                break;
-            case sf::Event::KeyPressed:
-                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
-                if(e.key.code == sf::Keyboard::Escape)
-                    shouldExit = true;
-                break;
-            default:
-                break;
-            }
-        }
-        if(shouldExit) {
-            window.close();
-            break;
-        }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
-
-        window.clear();
-        window.display();
-    }
+    Playlist Favorites{"Favorites", songs};
+    Favorites.calculateLength();
+    std::cout << Favorites.getLength() << std::endl;
     return 0;
 }
