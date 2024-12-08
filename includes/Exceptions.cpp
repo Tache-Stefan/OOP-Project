b561fca2fc96dbe7cd6b2cee68a55f5660fb1fa4
Tache-Stefan <stefantache@ymail.com>
2024-12-08 19:30:39 +0200

    Solved string issues.

--- includes/Exceptions.cpp
+++ includes/Exceptions.cpp
@@ -1,9 +1,9 @@
 #include "Exceptions.h"
 
-SearchException::SearchException(const std::string& msg) : BasicException("Search query is invalid: " + msg) {}
+SearchException::SearchException(const std::string& msg) : BasicException(std::string("Search query is invalid: ") + msg) {}
 
-FileRemoverException::FileRemoverException(const std::string& file) : BasicException("File remove is invalid: " + file) {}
+FileRemoverException::FileRemoverException(const std::string& file) : BasicException(std::string("File remove is invalid: ") + file) {}
 
 JsonException::JsonException() : BasicException("JSON error!") {}
 
-PlaylistException::PlaylistException(const std::string& name) : BasicException("Error creating playlist: " + name) {}
+PlaylistException::PlaylistException(const std::string& name) : BasicException(std::string("Error creating playlist: ") + name) {}
